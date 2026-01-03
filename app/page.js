1f8b9c30bf62dee7db3fa76bcc8f69edeae9ee3f
Dipesh-433 <deep20051113@gamil.com>
2026-01-03 17:28:40 +0530

    Authentication

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
-import { Button } from "@/components/ui/button";
+
+
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
      <h2>Hello world</h2>
-     <Button>h</Button>
+     <button className="px-4 py-2 bg-blue-600 text-white rounded">
+  Click Me
+</button>
+<UserButton />
    </div>
+   
   );
 }
